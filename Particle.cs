8cde909525b931ee674259ccc1124ccc7d7eb9df
Autumn McKee <autumn@mck.is>
2024-02-01 20:46:33 +0000

    Switch to dotnet 6

---
+++ Particle.cs
@@ -0,0 +1,15 @@
+namespace ParticleSim;
+
+public class Particle
+{
+    public Material Material { get; set; }
+    public float Timer { get; set; }
+    public bool HasBeenUpdated { get; set; }
+
+    public Particle(Material _material)
+    {
+        Material = _material;
+        Timer = 0f;
+        HasBeenUpdated = false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new ParticleSim.Game();
+game.Run();
